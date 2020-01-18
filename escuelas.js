df81232d08e96fc884542b05e8cff03f5721bc81
Ricardo Mora <rurimoca@gmail.com>
2020-01-18 15:50:51 -0600

    Carga de archivos a GitHub

---
+++ escuelas.js
@@ -0,0 +1,28 @@
+class Univerdad
+{
+  constructor(nombre, colegiatura, carreras, locacion)
+  {
+    this.escuela = nombre;
+    this.colegiatura = colegiatura;
+    this.carreras = carreras;
+    this.locacion = locacion;
+  }
+}
+
+var tecMonterrey =
+{
+  nombre: "Tec de Monterrey",
+  carrerasTotales: ["Medicina", "Sistemas Computacionales", "Mercadotecnia"],
+  locaciones: ["Acueducto", "La Raza", "La Roma"],
+  carrera:
+};
+
+var escuelas = [];
+escuelas.push( new Univerdad(tecMonterrey.nombre, "20,000", ["Medicina", "Sistemas Computacionales", "Mercadotecnia"], ["Acueducto", "La Raza", "La Roma"] ) );
+
+
+
+function electorCarrera()
+{
+
+}
